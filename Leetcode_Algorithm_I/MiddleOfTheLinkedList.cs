dee48340f1b0e8f9745a91a1494a94cf7237f16c
RamziHamouda <43704197+RamziHamouda@users.noreply.github.com>
2022-09-17 14:03:01 +0100

    Add Middle of the linked list solution

---
+++ Leetcode_Algorithm_I/MiddleOfTheLinkedList.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Leetcode_Algorithm_I
+{
+    public class ListNode
+    {
+        public int val;
+        public ListNode next;
+
+        public ListNode(int val = 0, ListNode next = null)
+        {
+            this.val = val;
+            this.next = next;
+
+        }
+
+        public static ListNode MiddleNode(ListNode head)
+        {
+            var length = 1;
+            ListNode node = new ListNode(head.val, head.next);
+            while (node.next != null)
+            {
+                length += 1;
+                node = node.next;
+            }
+
+            node = new ListNode(head.val, head.next);
+            for (var i = 1; i <= length/2; i ++)
+            {
+                node = node.next;
+            }
+
+            return node;
+        }
+
+        public static void test()
+        {
+            var listNode = new ListNode(5);
+            var listNode2 = new ListNode(4, listNode);
+            var listNode3 = new ListNode(3, listNode2);
+            var listNode4 = new ListNode(2, listNode3);
+            var listNode5 = new ListNode(1, listNode4);
+
+            var result = ListNode.MiddleNode(listNode5);
+
+            Console.WriteLine("Result = {0}", result.val);
+        }
+    }
+}
