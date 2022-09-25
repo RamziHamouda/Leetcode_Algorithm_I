4737260ff4762a6880750ddc8cce872318cd5466
RamziHamouda <43704197+RamziHamouda@users.noreply.github.com>
2022-09-25 11:25:06 +0100

    Solve Merge Two Sorted Lists problem

---
+++ Leetcode_Algorithm_I/Easy/MergeTwoSortedLists.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Leetcode_Algorithm_I.Easy
+{
+    public class MergeTwoSortedLists
+    {
+        public static ListNode MergeTwoSortedLists_Method(ListNode list1, ListNode list2)
+        {
+            var list = new List<int>();
+
+            while (list1 != null && list2 != null)
+            {
+                if (list1.val <= list2.val)
+                {
+                    list.Add(list1.val);
+                    list1 = list1.next;
+                }
+                else
+                {
+                    list.Add(list2.val);
+                    list2 = list2.next;
+                }
+            }
+
+            while (list1 != null)
+            {
+                list.Add(list1.val);
+                list1 = list1.next;
+            }
+
+            while (list2 != null)
+            {
+                list.Add(list2.val);
+                list2 = list2.next;
+            }
+
+            list.ForEach(x => Console.WriteLine(x));
+
+            ListNode head = null;
+            if (list.Count > 0) head = new ListNode();
+
+            var result = head;
+
+            for (int i = 0; i < list.Count; i++)
+            {
+                result.val = list[i];
+                if (i != list.Count - 1)
+                {
+                    result.next = new ListNode();
+                    result = result.next;
+                }
+            }
+
+            // Be careful here: if the two lists are null, the result must be null
+            // not new ListNode() because that will get back 0 (default of int)
+            return head;
+        }
+    }
+}
+
+
+/*
+ * var result = MergeTwoSortedLists.MergeTwoSortedLists_Method(null, null);
+
+while(result != null)
+{
+    Console.WriteLine(result.val);
+    result = result.next;
+}
+*/
--- Leetcode_Algorithm_I/Program.cs
+++ Leetcode_Algorithm_I/Program.cs
@@ -1,9 +1,18 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using Leetcode_Algorithm_I;
+using Leetcode_Algorithm_I.Easy;
 using Leetcode_Algorithm_I.Medium;
 using System.Linq;
 
 Console.WriteLine("Hello, World!");
+
+var result = MergeTwoSortedLists.MergeTwoSortedLists_Method(null, null);
+
+while(result != null)
+{
+    Console.WriteLine(result.val);
+    result = result.next;
+}
 
 Console.WriteLine("Result = /n");
 
