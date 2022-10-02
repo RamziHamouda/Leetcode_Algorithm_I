0700355f9a31119c309059d3c68e0ac00cda8101
RamziHamouda <43704197+RamziHamouda@users.noreply.github.com>
2022-10-02 18:35:20 +0100

    Solve Flood Fill problem

---
+++ Leetcode_Algorithm_I/Easy/FloodFill.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Leetcode_Algorithm_I.Easy
+{
+    internal class FloodFillClass
+    {
+        public int[][] FloodFill(int[][] image, int sr, int sc, int color)
+        {
+
+            if (image[sr][sc] == color) return image;
+
+            var previousColor = image[sr][sc];
+            image[sr][sc] = color;
+
+            if (sc - 1 >= 0)
+                if (image[sr][sc - 1] == previousColor) FloodFill(image, sr, sc - 1, color);
+
+            if (sr - 1 >= 0)
+                if (image[sr - 1][sc] == previousColor) FloodFill(image, sr - 1, sc, color);
+
+            if (sr + 1 < image.Length)
+                if (image[sr + 1][sc] == previousColor) FloodFill(image, sr + 1, sc, color);
+
+            if (sc + 1 < image[sr].Length)
+                if (image[sr][sc + 1] == previousColor) FloodFill(image, sr, sc + 1, color);
+
+            return image;
+        }
+    }
+}
