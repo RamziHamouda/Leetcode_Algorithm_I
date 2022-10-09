45fd334513702922cd7b46d8834bc7a36b4dd860
RamziHamouda <43704197+RamziHamouda@users.noreply.github.com>
2022-10-09 14:49:50 +0100

    Solve reverse string

---
+++ Leetcode_Algorithm_I/Easy/ReverseString.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Leetcode_Algorithm_I.Easy
+{
+    internal class ReverseStringClass
+    {
+        public void ReverseString(char[] s)
+        {
+            int i = 0;
+            int j = s.Length - 1;
+            char p = ' ';
+
+            while (i < j)
+            {
+                p = s[i];
+                s[i] = s[j];
+                s[j] = p;
+
+                i++;
+                j--;
+            }
+        }
+    }
+}
