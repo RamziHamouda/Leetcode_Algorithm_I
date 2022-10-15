0dcefe2ba2b796bf093c2547e05a9206d126f703
RamziHamouda <43704197+RamziHamouda@users.noreply.github.com>
2022-10-15 18:49:32 +0100

    solve number of 1 bits

---
+++ Leetcode_Algorithm_I/Easy/NumberOf1Bits.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Leetcode_Algorithm_I.Easy
+{
+    public class NumberOf1BitsClass
+    {
+        public int NumberOf1Bits(uint n)
+        {
+
+            var myBitArray = new BitArray(BitConverter.GetBytes(n));
+
+            int result = 0;
+            foreach (bool element in myBitArray)
+            {
+                if (element == true) result++;
+
+            }
+
+            return result;
+
+        }
+    }
+}
